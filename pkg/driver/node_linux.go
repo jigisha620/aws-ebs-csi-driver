08ccb67318898512cad4543d8a036932800cdbaf
Patil <patixjig@bcd0741b7bce.ant.amazon.com>
2022-07-13 17:04:34 -0700

    Updated Dockerfile to point to Amazon Linux Image

--- pkg/driver/node_linux.go
+++ pkg/driver/node_linux.go
@@ -20,9 +20,9 @@ limitations under the License.
 package driver
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
-	osexec "os/exec"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -106,16 +106,14 @@ func (d *nodeService) findDevicePath(devicePath, volumeID, partition string) (st
 
 	klog.V(5).Infof("[Debug] Falling back to snow volume lookup for: %q", devicePath)
 
-	//snowDevicePath, err := d.deviceIdentifier.FindSnowVolume()
-	//snowDevicePath, err := d.deviceIdentifier.Lstat("/dev/vda")
-	snowDevicePath, err := findSnowVolume(d, err)
-
-	//if err == nil {
-	//	klog.V(5).Infof("[Debug] successfully resolved devicePath=%q to %q", devicePath, snowDevicePath)
-	//	canonicalDevicePath = snowDevicePath
-	//} else {
-	//	klog.V(5).Infof("[Debug] error searching for snow path: %v", err)
-	//}
+	//snowDevicePath, err := findSnowVolume(d, err)
+	snowDevicePath, err := d.deviceIdentifier.FindSnowVolume()
+	if err == nil {
+		klog.V(5).Infof("[Debug] successfully resolved devicePath=%q to %q", devicePath, snowDevicePath)
+		canonicalDevicePath = snowDevicePath
+	} else {
+		klog.V(5).Infof("[Debug] error searching for snow path: %v", err)
+	}
 
 	if canonicalDevicePath == "" {
 		return "", errNoDevicePathFound(devicePath, volumeID, snowDevicePath, err)
@@ -125,34 +123,31 @@ func (d *nodeService) findDevicePath(devicePath, volumeID, partition string) (st
 	return canonicalDevicePath, nil
 }
 
-func findSnowVolume(d *nodeService, err error) ([]byte, error) {
-	//snowDevicePath := ""
-	//cmd := d.mounter.(*NodeMounter).Exec.Command("/usr/bin/lsblk", "--json")
-	//output, err := cmd.Output()
-	cmd := osexec.Command("lsblk", "--json")
+func findSnowVolume(d *nodeService, err error) (string, error) {
+	snowDevicePath := ""
+	cmd := d.mounter.(*NodeMounter).Exec.Command("lsblk", "--json", "--output", "NAME,MOUNTPOINT")
 	output, err := cmd.Output()
-	//rawOut := make(map[string][]BlockDevice, 1)
-	//err = json.Unmarshal(output, &rawOut)
-	//if err != nil {
-	//	klog.V(5).Infof("unable to unmarshal output to BlockDevice instance, error: %v", err)
-	//}
-	//var (
-	//	devs []BlockDevice
-	//	ok   bool
-	//)
-	//if devs, ok = rawOut["blockdevices"]; !ok {
-	//	klog.V(5).Infof("unexpected lsblk output format, missing block devices")
-	//}
-	//for _, d := range devs {
-	//	if (strings.HasPrefix(d.Name, "/dev/v")) && (len(d.MountPoint) == 0) {
-	//		snowDevicePath = d.Name
-	//	}
-	//}
-	//return snowDevicePath, err
-	return output, err
+	rawOut := make(map[string][]BlockDevice, 1)
+	err = json.Unmarshal(output, &rawOut)
+	if err != nil {
+		klog.V(5).Infof("unable to unmarshal output to BlockDevice instance, error: %v", err)
+	}
+	var (
+		devs []BlockDevice
+		ok   bool
+	)
+	if devs, ok = rawOut["blockdevices"]; !ok {
+		klog.V(5).Infof("unexpected lsblk output format, missing block devices")
+	}
+	for _, d := range devs {
+		if (strings.HasPrefix(d.Name, "/dev/v")) && (len(d.MountPoint) == 0) {
+			snowDevicePath = d.Name
+		}
+	}
+	return snowDevicePath, err
 }
 
-func errNoDevicePathFound(devicePath string, volumeID string, snowDevicePath []byte, err error) error {
+func errNoDevicePathFound(devicePath string, volumeID string, snowDevicePath string, err error) error {
 	return fmt.Errorf("no device path for device %q volume %q found snowdevicePath %v errorMount %v", devicePath, volumeID, snowDevicePath, err)
 }
 
